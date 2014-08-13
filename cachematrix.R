cab5d65cbbdb1e9a8f4379f04a70351c1f4c284c
duncan6152 <duncan.6152@gmail.com>
2014-08-13 17:45:15 +0800

    second corrected version

--- cachematrix.R
+++ cachematrix.R
@@ -1,121 +1,118 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
-#  fnlist <- makeCacheMatrix(x) stores a matrix, x, and its inverse.
-#  and it returns a list of functions, fnlist, to access these.
+##----------------------
+#  A call   fnlist <- makeCacheMatrix(x)  creates an environment
+#     containing the matrix, x, and its inverse xinv.
+#  It returns a list of functions 
+#   ie,  fnlist <- list( get, set, getinv, setinv)
+#  which can be used by a function such as cacheSolve
+#  to access x and xinv
 #
-#  cacheSolve(fnlist) returns the inverse of the matrix x stored
-#    by the call fnlist <- makeCacheMatrix(x)
-#  If the inverse does not exist it first calculates and caches it.
-#  If the matrix is singular an error message is printed  
+#  Then a call xinv <- cacheSolve(fnlist), using this function list,  
+#  returns the inverse of the matrix x stored in the environment of
+#    makeCacheMatrix(x)
+#  If the inverse does not exist it is first calculated and cached,
+##-----------------------
 
 ## Write a short comment describing this function
-
-## A call to makeCacheMatrix creates an environment
-#    which contains a copy, x, of the matrix argument
+##-----------------------------------------------
+## A call to makeCacheMatrix(x) creates an environment
+#    which contains a copy of the matrix argument x.
+## The environment also contains a local variable, xinv, which 
+#    is initially NULL but will be set to the inverse of x.
 ## This environment persists.
-## The environment contains a local variable, xinv, 
-#  which is initially NULL.
-#
-## The function returns a list of functions (defined in
-#    makeCacheMatrix) which access x and xinv.
-## These functions can be used by a function such as 
-#    cacheSolve to set and get the values of xinv and x
+## It returns a list of functions which access x and xinv.
 
 ## A sample call is 
-#    inv1 <- makeCacheMatrix(matrix(1:9, 3, 3)) 
+#   fnlist <- makeCacheMatrix(matrix(1:9, 3, 3)) 
 #   which initialises x to matrix(1:9, 3, 3)
 #   and sets xinv to NULL
+##------------------------
 
 makeCacheMatrix <- function(x = matrix()) {
     xinv <- NULL
-    ## set - resets of the environment values
+  ## the enviromnent stores x and xinv
+
+  ## the following functions manipulate x and xinv
+  ##   (using <<- to access the defining environment)
+  ## set - resets the defining environment values
     set <- function(y) {
-              ## change the matrix x to y - DANGEROUS
               x <<- y
-              xinv <- NULL
+              xinv <<- NULL
            }  
-    ## return the matrix being inverted 
+  ## return the matrix being inverted 
     get <- function() x
-    ## set the inverse xinv to a new matrix yinv
-    ## TAKE CARE that yinv is the inverse of x
+  ## reset xinv to a new matrix yinv
     setinv <- function(yinv)  xinv <<- yinv
-    ## return the inverse of x
+  ## return the inverse of x
     getinv <- function() xinv 
 
-    ## return a list of the 4 functions to access x, xinv
-    ##   as free variables and use <<- to assign to them
+  ## return a list of the 4 functions to access x, xinv
     list( set= set,         get= get, 
           setinv = setinv,  getinv= getinv
         )
 }
 
 ## Write a short comment describing this function
-
+##-----------------------------------------------
 ## The first argument to cacheSolve is a list of functions
-#    returned by a call such as  mm <- makeCacheMatrix,
+#    returned by a call such as  fnlist <- makeCacheMatrix(x).
+#    These functions access a matrix, x, and its inverse xinv 
+#    in the environment created by makeCacheMatrix(x).
 #
-## The call,   cacheSolve(mm)
-#    where mm is a list of these functions,
-#    eg:  mm$set, mm$get,  mm$setinv, mm$getinv
-#  is used initialise the inverse of mm$xinv if it is NULL
-#    or return its value if non-NULL.
-#
-##  Typically the first call finds the inverse is NULL so it 
-#    calculates the inverse and caches it.
-#   Subsequent calls just returns the cached inverse
+## The call, inv <- cacheSolve(fnlist), where fnlist is the list 
+#  of   fnlist$set, fnlist$get,  fnlist$setinv, fnlist$getinv,
+#  is used get the inverse of matrix x, using fnlist$getinv(), 
+#  and return its value.  
+# 
+#  Typically the first call finds the inverse to be NULL so it 
+#  then calculates the inverse and caches it.
+#  Subsequent calls just return the cached inverse.
+##-------------------------
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     ## x is a list of functions which access the matrix
+     ## Return the inverse of the matrix
 
         # get local copy of the inverse
-        xinv <- x$getinv()
-        if( !is.null(xinv)) {  ## the inverse exists
+        inv <- x$getinv()
+        if( !is.null(inv)) {  ## the inverse exists
             ## message("getting cached inverse")
-            return( xinv)
+            return( inv)
         }
         # else get the matrix and calculate the inverse
         mat <- x$get()         ## get the matrix
-        xinv <- solve( mat )   ## get its inverse
-        x$setinv( xinv )       ## cache the inverse
-        xinv                   ## return the inverse         
+        inv <- solve( mat )   ## get its inverse
+        x$setinv( inv )       ## cache the inverse
+        inv                   ## return the inverse         
 }
 
+##-----------------------------------------------------
 ## Usage
-## makeCacheMatrix(x) creates an environment 
+## fnlist <- makeCacheMatrix(x)  creates an environment 
 #     containing argument x initialised to the data matrix 
-#     and a local variable xinv tho hold the inverse. 
-#   and returns list of functions to manipulate these
-## The environment, with x and xinv, persists
+#     and a local variable xinv to hold the inverse. 
+#  It returns a list of functions to manipulate these.
 #
 ##  Sample call:
-#   mm <- makeCacheMatrix(matrix(1:4,2,2))
+#   fnlist <- makeCacheMatrix(matrix(1:4,2,2))
 #   initialises x to matrix(1:4,2,2) and xinv to NULL
-#   mm is a list of the functions returned and accessed as
-#    mm$get(), mm$set(y), mm$setinv(yinv), mm$getinv()
-
-## The call
-#     cacheSolve(mm) 
-#  is used initialise the inverse of mm$xinv if it is NULL
-#    or return its value if non-NULL.
-#  Typically the first call finds mm$xinv is NULL so it 
-#    calculates the inverse and caches it.
-#  Subsequent calls just return the non-NULL mm$xinv
+#
+## Then the call
+#  inv <- cacheSolve(fnlist) 
+#  is used calculate and store the inverse if it is NULL
+#  It returns the value of the inverse.
 
 ## Another call,
 #      m2 <- makeCacheMatrix(matrix(c(1:5,4:7),3,3))
 #  creates a separate environment for this matrix and 
-#  and returns its functions in list m2.   The cached inverse
-#  is then returned by the call:  cacheSolve( m2 )
-
-## NOTE the value of x should not be set by cacheSolve
-#    as it is input as the argument of makeCacheMatrix.
-#  WARNING  Other functions could be written to change x 
-#    and xinv to corrupt these essentially private values
+#  and returns its functions to list m2.   The cached inverse
+#  is then returned by the call:  m2inv <- cacheSolve( m2 )
 
 ## Test
 # n <- 300
 # big <- matrix(sample(1:2000,n*n, replace=TRUE), n, n)
 # m2 <- makeCacheMatrix(big)
 # mxinv <- cacheSolve(m2)
-# big%*%mxinv
+# summary(big%*%mxinv)    ## check identity matrix
+
